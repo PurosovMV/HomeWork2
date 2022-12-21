d7c313ed4182c928d2fa8b2938a1ec3656d6b8ad
PurosovMV <mihail.purosov@hotmail.com>
2022-12-21 20:57:08 +0300

    Решена задача №1

---
+++ Задача1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.WriteLine("Введи трёхзначное число: ");
+int input = int.Parse(Console.ReadLine());
+if (input > 0)
+{
+Console.WriteLine((input%100-input%10)/10);
+}
+if (input < 0)
+{
+
+Console.WriteLine((-1*(input%100-input%10)/10));
+}
